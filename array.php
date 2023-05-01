abf8dd66f1d8574c4fa55c81de033448fe40982e
Jonas-Hemmett <123014493+Jonas-Hemmett@users.noreply.github.com>
2023-05-01 18:33:39 -0400

    Add files via upload

--- array.php
+++ array.php
@@ -2,77 +2,72 @@
 include "top.php";
 ?>
 <main>
-    <section class="flexWide">
+    <section class="flexWide flexList">
         <h3>Mystery Inc. Members</h3>
         <p>
             Here is a complete list of the 5 core Mystery Gang Members!
         </p>
-        <table>
-            <tr>
-                <th colspan="2">Scooby-Doo</td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="resources/array/ScoobyDoo.png" alt="Scooby-Doo"></a>
-                </td>
-                <td> 
-                    Scooby-Doo is Shaggy’s pet dog, and the Mystery Team’s mascot. He is constantly hungry, scared easily and most of the time refuse to help others. He enjoys "Scooby Snacks", a biscuit-like dog treat or cookie snack (usually shaped like a bone). His catch phrase, usually howled at the end of every mystery, is "Scooby-Dooby-Doo!" or "Rooby-Rooby-Roo". He is able to talk in english while pronouncing most words as if they begin with an "R."
-                </td>
-            </tr>
+            
+        <section>
+            <h4>
+                Scooby-doo
+            </h4>
+            <figure>
+                <img src="resources/array/ScoobyDoo.png" alt="Scooby-Doo">
+            </figure>
+            <p>
+                Scooby-Doo is Shaggy’s pet dog, and the Mystery Team’s mascot. He is constantly hungry, scared easily and most of the time refuse to help others. He enjoys "Scooby Snacks", a biscuit-like dog treat or cookie snack (usually shaped like a bone). His catch phrase, usually howled at the end of every mystery, is "Scooby-Dooby-Doo!" or "Rooby-Rooby-Roo". He is able to talk in english while pronouncing most words as if they begin with an "R."
+            </p>
+        </section>
 
+        <section>
+            <h4>
+                Shaggy Rogers
+            </h4>
+            <figure> 
+                <img src="resources/array/ShaggyRogers.png" alt="Shaggy Rogers">
+            </figure>
+            <p>
+                Shaggy is a hippy member of the Mystery Gang with a laid back personality. He frequently slips in the word "Like" in his speech and uses his catch phrase "Zoinks!" whenever he's surprised or scared. He known to frequently be hungry, is able to eat range quantities of food and can easily be persuaded by Scooby Snacks
+            </p>
+        </section>
 
-            <tr>
-                <th colspan="2">Shaggy Rogers</td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="resources/array/ShaggyRogers.png" alt="Shaggy Rogers"></a>
-                </td>
-                <td>
-                    Shaggy is a hippy member of the Mystery Gang with a laid back personality. He frequently slips in the word "Like" in his speech and uses his catch phrase "Zoinks!" whenever he's surprised or scared. He known to frequently be hungry, is able to eat range quantities of food and can easily be persuaded by Scooby Snacks
-                </td>
-            </tr>
+        <section>
+            <h4>
+                Fred Jones
+            </h4>
+            <figure>
+                <img src="resources/array/FredJones.png" alt="Fred Jones" class="centerImg">
+            </figure>
+            <p>
+                Fred is known as the leader of the Mystery Inc. Gang, and is usually the driver of their van, The Mystery Machine. Fred is statuesque and brave an dis usually seen wearing a white sweater over a blue shirt with his trademark accessory, his orange ascot. His catch phrase is something along the lines of, "Another mystery solved", "Let's split up" or "looks like we have a(nother) mystery on our hands." 
+            </p>
+        </section>
 
-            <tr>
-                <th colspan="2">Fred Jones</td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="resources/array/FredJones.png" alt="Fred Jones"></a>
-                </td>
-                <td>
-                    Fred is known as the leader of the Mystery Inc. Gang, and is usually the driver of their van, The Mystery Machine. Fred is statuesque and brave an dis usually seen wearing a white sweater over a blue shirt with his trademark accessory, his orange ascot. His catch phrase is something along the lines of, "Another mystery solved", "Let's split up" or "looks like we have a(nother) mystery on our hands."
-                </td>
-            </tr>
+        <section>
+            <h4>
+                Daphne Blake
+            </h4>
+            <figure>
+                <img src="resources/array/DaphneBlake.png" alt="Daphne Blake" class="centerImg">
+            </figure>
+            <p>
+                Daphne is a rich, fashion-loving member of Mystery Gang. Daphne's characteristic trait in the gang is her tendency to get into danger, but is still quite formidable. She has a black belt in Karate, can use different her make-up accessories to open locked doors and cages, is very flexible, and can think quickly during a fight. She is usually seen wearing a purple dress with a green scarf and light purple headband.
+            </p>
+        </section>
 
-            <tr>
-                <th colspan="2">Daphne Blake</td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="resources/array/DaphneBlake.png" alt="Daphne Blake"></a>
-                </td>
-                <td>
-                    Daphne is a rich, fashion-loving member of Mystery Gang. Daphne's characteristic trait in the gang is her tendency to get into danger, but is still quite formidable. She has a black belt in Karate, can use different her make-up accessories to open locked doors and cages, is very flexible, and can think quickly during a fight. She is usually seen wearing a purple dress with a green scarf and light purple headband.
-                </td>
-            </tr>
+        <section>
+            <h4>
+                Velma Dinkley
+            </h4>
+            <figure>
+                <img src="resources/array/VelmaDinkley.png" alt="Velma Dinkley"></a>
+            </figure>
+            <p>
+                Velma is known as the most intelligent member of the Mystery Gang.She usually wears a orange shirt and red skirt and is often the one to decipher the clues and solve the crimes.She is sharp-witted and sometimes sarcastic. She frequently loses her glasses  and immediately claims,"My glasses. I can't find my glasses" or "I can't see without my glasses," before crawling around on the floor until she finds them.   
+            </p>
+        </section>
 
-            <tr>
-                <th colspan="2">Velma Dinkley</td>
-            </tr>
-            <tr>
-                <td>
-                    <img src="resources/array/VelmaDinkley.png" alt="Velma Dinkley"></a>
-                </td>
-                <td>
-                    Velma is known as the most intelligent member of the Mystery Gang.She usually wears a orange shirt and red skirt and is often the one to decipher the clues and solve the crimes.She is sharp-witted and sometimes sarcastic. She frequently loses her glasses  and immediately claims,"My glasses. I can't find my glasses" or "I can't see without my glasses," before crawling around on the floor until she finds them.               
-                </td>
-            </tr>
-
-        </table> 
-    </section>
-    <section>
-               
     </section>
 </main>
     
--- resources/Trash/detail.php
+++ resources/Trash/detail.php
@@ -2,71 +2,36 @@
 include "top.php";
 ?>
 <main>
-    <section class="flexWide flexList">
+    <section class="flexWide">
         <h3>Villains</h3>
         <p>
             Scooby-Doo is famous for its seemingly supernatural villains, featuring a different one in almost every episode. Most of the villains are petty criminals who disguise themselves as monsters to get away with their various crimes. Listed below are five of our favorites
         </p>
             
-        <section>
-            <h4>
-                The Ghost of Dr. Coffin   
-            </h4>
-            <figure>
-                <img src="resources/detail/TheGhostOfDrCoffin.png" alt="The Ghost of Dr. Coffin" class="centerImg">
-            </figure>
+            <p><b><img src="resources/detail/TheGhostOfDrCoffin.png" alt="The Ghost of Dr. Coffin"></b><p>
             <p>
                 The Ghost of Dr. Coffin seemed to be an undead Doctor haunting the Shady Sanitarium in Niagara falls. He had pale skin, green eyes, and a glowing aura. As it turned out the ghost of Dr. Coffin wasn’t real, but simply a disguise for Officer Oldfield. Officer Oldfield was smuggling gold and needed to keep his operations under wraps.
             </p>
-        </section>
 
-        <section>
-            <h4>
-                Captain Skunkbeard  
-            </h4>
-            <figure> 
-                <img src="resources/detail/CaptainSkunkBeard.png" alt="Captain Skunkbeard" class="centerImg">
-            </figure>
+            <p><b> <img src="resources/detail/CaptainSkunkBeard.png" alt="Captain Skunkbeard"></b><p>
             <p>
                 Captain Skunkbeard is a supposed ghost pirate. He has green skin, red clothes, and a big gray beard. He was extremely terrifying. His true identity is Biff Wellington, an eccentric billionaire. Biff Wellington was trying to get Heaven's Light, a meteor in the Bermuda triangle in hopes of getting its supposed time traveling abilities. He disguised himself as Captain Skunkbeard as an attempt to get closer to the center of the triangle 
             </p>
-        </section>
-
-        <section>
-            <h4>
-                The Mystery Machine  
-            </h4>
-            <figure>
-                <img src="resources/detail/TheMysteryMachine.png" alt="The Mystery Machine" class="centerImg">
-            </figure>
+            
+            <p><b><img src="resources/detail/TheMysteryMachine.png" alt="The Mystery Machine"></b><p>
             <p>
                 The Mystery Machine, The gang's trusty blue and green van, would become seemingly haunted and turn on the gang on a few occasions. The first of which was caused by Susan Dinwiddie in an attempt to get publicity for her band, she used her son's robotic tools and knowledge to control the van remotely. The second time the van was controlled by Dr. Blum, a scientist who worked at the  Meldon Archeological Science Laboratory, rigged the van with a remote control so he could control it. He did it to get rid of Fred and secure a security contract. 
             </p>
-        </section>
 
-        <section>
-            <h4>
-            Professor Pericles 
-            </h4>
-            <figure>
-                <img src="resources/detail/ProfessorPericles.png" alt="Professor Pericles" class="centerImg">
-            </figure>
+            <p><b><img src="resources/detail/ProfessorPericles.png" alt="Professor Pericles"></b><p>
             <p>
-            Professor Pericles was the mascot for the original Mystery Inc. He is a small blue parrot with human-like intelligence similar to Scooby-Doo. Despite being part of the Mystery Inc. He betrayed them in a vain attempt to seize power which ended up putting him in the Crystal Cove Animal Asylum for the Criminally Insane. He later would escape and battle the current Mystery Inc and die after failing to overpower them. 
+                Professor Pericles was the mascot for the original Mystery Inc. He is a small blue parrot with human-like intelligence similar to Scooby-Doo. Despite being part of the Mystery Inc. He betrayed them in a vain attempt to seize power which ended up putting him in the Crystal Cove Animal Asylum for the Criminally Insane. He later would escape and battle the current Mystery Inc and die after failing to overpower them. 
             </p>
-        </section>
 
-        <section>
-            <h4>
-                The Black Night  
-            </h4>
-            <figure>
-                <img src="resources/detail/TheBlackKnight.png" alt="The Black Night" class="centerImg">
-            </figure>
+            <p><b><img src="resources/detail/TheBlackKnight.png" alt="The Black Night"></a></b>
             <p>
                 The black knight was a tall suit of armor with a red plume and glowing green eye. Legend has it that the black night  comes alive during a full moon. Despite its seemingly supernatural abilities the Black Knight is just the curator of the county museum Mr. Wickles in disguise. Mr. Wickles was planning on replacing the paintings in the county museum with forgies and selling the originals. He dressed up as the Black Knight to scare away anyone who wanted to stop his plan. 
             </p>
-        </section>
 
     </section>
 </main>
