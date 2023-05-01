38b58bdcaaaffaddc95d3a66465f9e87bb1a5e63
Jonas-Hemmett <123014493+Jonas-Hemmett@users.noreply.github.com>
2023-05-01 09:31:35 -0400

    Add files via upload

--- array.php
+++ array.php
@@ -16,12 +16,7 @@ include "top.php";
                     <img src="resources/array/ScoobyDoo.png" alt="Scooby-Doo"></a>
                 </td>
                 <td> 
-                Scooby-Doo is Shaggy’s pet dog, and the Mystery Team’s mascot. 
-                He is constantly hungry, scared easily and most of the time refuse to help others.
-                 He enjoys "Scooby Snacks", a biscuit-like dog treat or cookie snack 
-                 (usually shaped like a bone). His catch phrase, usually howled at the end of every mystery, 
-                 is "Scooby-Dooby-Doo!" or "Rooby-Rooby-Roo". 
-                 He is able to talk in english while pronouncing most words as if they begin with an "R."
+                    Scooby-Doo is Shaggy’s pet dog, and the Mystery Team’s mascot. He is constantly hungry, scared easily and most of the time refuse to help others. He enjoys "Scooby Snacks", a biscuit-like dog treat or cookie snack (usually shaped like a bone). His catch phrase, usually howled at the end of every mystery, is "Scooby-Dooby-Doo!" or "Rooby-Rooby-Roo". He is able to talk in english while pronouncing most words as if they begin with an "R."
                 </td>
             </tr>
 
@@ -34,10 +29,7 @@ include "top.php";
                     <img src="resources/array/ShaggyRogers.png" alt="Shaggy Rogers"></a>
                 </td>
                 <td>
-                Shaggy is a hippy member of the Mystery Gang with a laid back personality. 
-                He frequently slips in the word "Like" in his speech and uses his catch phrase "Zoinks!" 
-                whenever he's surprised or scared. He known to frequently be hungry, is able to eat range quantities of 
-                food and can easily be persuaded by Scooby Snacks.
+                    Shaggy is a hippy member of the Mystery Gang with a laid back personality. He frequently slips in the word "Like" in his speech and uses his catch phrase "Zoinks!" whenever he's surprised or scared. He known to frequently be hungry, is able to eat range quantities of food and can easily be persuaded by Scooby Snacks
                 </td>
             </tr>
 
@@ -49,10 +41,7 @@ include "top.php";
                     <img src="resources/array/FredJones.png" alt="Fred Jones"></a>
                 </td>
                 <td>
-                Fred is known as the leader of the Mystery Inc. Gang, and is usually the driver of their van, 
-                The Mystery Machine. Fred is statuesque and brave an dis usually seen wearing a white sweater over 
-                a blue shirt with his trademark accessory, his orange ascot. His catch phrase is something a;ong the lines of, 
-                "Another mystery solved", "Let's split up" or "looks like we have a(nother) mystery on our hands."
+                    Fred is known as the leader of the Mystery Inc. Gang, and is usually the driver of their van, The Mystery Machine. Fred is statuesque and brave an dis usually seen wearing a white sweater over a blue shirt with his trademark accessory, his orange ascot. His catch phrase is something along the lines of, "Another mystery solved", "Let's split up" or "looks like we have a(nother) mystery on our hands."
                 </td>
             </tr>
 
@@ -64,10 +53,7 @@ include "top.php";
                     <img src="resources/array/DaphneBlake.png" alt="Daphne Blake"></a>
                 </td>
                 <td>
-                Daphne is a rich, fashion-loving member of Mystery Gang. Daphne's characteristic trait in the gang 
-                is her tendency to get into danger, but is still quite formidable. She has a black belt in Karate, can use different her 
-                make-up accessories to open locked doors and cages, is very flexible, and can think quickly during a fight. 
-                She is usually seen wearing a purple dress with a green scarf and light purple headband.
+                    Daphne is a rich, fashion-loving member of Mystery Gang. Daphne's characteristic trait in the gang is her tendency to get into danger, but is still quite formidable. She has a black belt in Karate, can use different her make-up accessories to open locked doors and cages, is very flexible, and can think quickly during a fight. She is usually seen wearing a purple dress with a green scarf and light purple headband.
                 </td>
             </tr>
 
@@ -79,11 +65,7 @@ include "top.php";
                     <img src="resources/array/VelmaDinkley.png" alt="Velma Dinkley"></a>
                 </td>
                 <td>
-                    Velma is known as the most intelligent member of the Mystery Gang. 
-                    She usually wears a orange shirt and red skirt and is often the one to decipher the clues and solve the crimes.
-                     She is sharp-witted and sometimes sarcastic. She frequently loses her glasses  and immediately claims, 
-                     "My glasses. I can't find my glasses" or "I can't see without my glasses," 
-                     before crawling around on the floor until she finds them.
+                    Velma is known as the most intelligent member of the Mystery Gang.She usually wears a orange shirt and red skirt and is often the one to decipher the clues and solve the crimes.She is sharp-witted and sometimes sarcastic. She frequently loses her glasses  and immediately claims,"My glasses. I can't find my glasses" or "I can't see without my glasses," before crawling around on the floor until she finds them.               
                 </td>
             </tr>
 
