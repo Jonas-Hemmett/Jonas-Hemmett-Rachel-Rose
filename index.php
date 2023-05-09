c4d3478855de2dec6ca66f0c1c49f4e378ef66f6
rachel-rose <113383769+rachel-rose@users.noreply.github.com>
2023-05-08 21:34:07 -0400

    Final Project: added display data to index.php

--- index.php
+++ index.php
@@ -87,6 +87,8 @@ include "top.php";
 
     <section>
         <h3>Showcase Video</h3>
+        <p>Coming Soon.</p>
+        
         <p>
             below is a video going over our website.
         </p>  
